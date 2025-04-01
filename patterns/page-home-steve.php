0c618f0cf68a37ab36c0710bf352030a836005ae
Eli Front <elifront23@gmail.com>
2025-03-31 22:43:30 -0500

    feat: WIP steve template

--- patterns/page-home-steve.php
+++ patterns/page-home-steve.php
@@ -30,46 +30,38 @@
     <!-- wp:button {"textColor":"base","className":"is-style-outline-background is-style-outline","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"fontSize":"small","borderColor":"base"} -->
     <div class="wp-block-button has-custom-font-size is-style-outline-background is-style-outline has-small-font-size">
       <a class="wp-block-button__link has-base-color has-text-color has-link-color has-border-color has-base-border-color wp-element-button"
-        href="#">Watch the video</a>
+        href="#campaign-video">Watch the video</a>
     </div>
     <!-- /wp:button -->
 
-    <!-- wp:button {"style":{"spacing":{"padding":{"left":"var:preset|spacing|medium","right":"var:preset|spacing|medium","top":"var:preset|spacing|small","bottom":"var:preset|spacing|small"}}},"fontSize":"small"} -->
+    <!-- wp:button {"fontSize":"small"} -->
     <div class="wp-block-button has-custom-font-size has-small-font-size"><a
-        class="wp-block-button__link wp-element-button" href="#signup-form"
-        style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--medium)">Join
-        the movement</a></div>
+        class="wp-block-button__link wp-element-button" href="#signup-form">Join the movement</a></div>
     <!-- /wp:button -->
   </div>
   <!-- /wp:buttons -->
 </div>
 <!-- /wp:group -->
 
-<!-- wp:group {"metadata":{"categories":["featured"],"patternName":"frost/hero-two-columns","name":"Section with image, text, buttons."},"align":"wide","layout":{"type":"default"}} -->
-<div class="wp-block-group alignwide">
+<!-- wp:group {"metadata":{"categories":["featured"],"patternName":"frost/hero-two-columns","name":"Section with image, text, buttons."},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|medium"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide" id="campaign-video" style="padding-top:var(--wp--preset--spacing--medium)">
   <!-- wp:columns {"verticalAlignment":"center","className":"is-style-columns-reverse"} -->
   <div class="wp-block-columns are-vertically-aligned-center is-style-columns-reverse">
     <!-- wp:column {"verticalAlignment":"center","width":""} -->
     <div class="wp-block-column is-vertically-aligned-center">
       <!-- wp:group {"layout":{"type":"constrained","wideSize":"480px","justifyContent":"left"}} -->
       <div class="wp-block-group"><!-- wp:heading {"className":"wp-block-heading","fontSize":"x-large"} -->
-        <h2 class="wp-block-heading has-x-large-font-size" id="text-on-left-image-on-right">Welcome to Frost</h2>
+        <h2 class="wp-block-heading has-x-large-font-size" id="text-on-left-image-on-right">Why Steve</h2>
         <!-- /wp:heading -->
 
         <!-- wp:paragraph -->
-        <p>With its clean, minimal design and powerful feature set, Frost enables agencies to build stylish and
-          sophisticated WordPress websites.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor ut labore et dolore magna aliqua.</p>
         <!-- /wp:paragraph -->
 
         <!-- wp:buttons -->
         <div class="wp-block-buttons"><!-- wp:button {"className":"is-style-fill"} -->
-          <div class="wp-block-button is-style-fill"><a class="wp-block-button__link wp-element-button">Get Started</a>
-          </div>
-          <!-- /wp:button -->
-
-          <!-- wp:button {"className":"is-style-outline"} -->
-          <div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">Learn
-              More</a></div>
+          <div class="wp-block-button is-style-fill"><a class="wp-block-button__link wp-element-button"
+              href="/about-steve">Learn More</a></div>
           <!-- /wp:button -->
         </div>
         <!-- /wp:buttons -->
@@ -79,139 +71,12 @@
     <!-- /wp:column -->
 
     <!-- wp:column {"verticalAlignment":"center","width":""} -->
-    <div class="wp-block-column is-vertically-aligned-center">
-      <!-- wp:image {"id":5592,"sizeSlug":"full","linkDestination":"none"} -->
-      <figure class="wp-block-image size-full"><img
-          src="http://localhost:8000/wp-content/themes/politics-wp-theme/assets/images/sample_1200x1200.jpg"
-          alt="Sample Image" class="wp-image-5592" /></figure>
-      <!-- /wp:image -->
-    </div>
-    <!-- /wp:column -->
-  </div>
-  <!-- /wp:columns -->
-</div>
-<!-- /wp:group -->
-
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|x-large","bottom":"var:preset|spacing|x-large","right":"30px","left":"30px"},"margin":{"top":"0"},"blockGap":"0"}},"layout":{"type":"constrained","wideSize":"960px"}} -->
-<div class="wp-block-group alignfull" id="features"
-  style="margin-top:0;padding-top:var(--wp--preset--spacing--x-large);padding-right:30px;padding-bottom:var(--wp--preset--spacing--x-large);padding-left:30px">
-  <!-- wp:group {"style":{"spacing":{"blockGap":"10px","padding":{"bottom":"var:preset|spacing|large"}}},"layout":{"type":"constrained","wideSize":"720px"}} -->
-  <div class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--large)">
-    <!-- wp:heading {"textAlign":"center","className":"wp-block-heading","style":{"typography":{"letterSpacing":"-1px"}},"fontSize":"max-60"} -->
-    <h2 class="wp-block-heading has-text-align-center has-max-60-font-size" id="text-on-left-image-on-right"
-      style="letter-spacing:-1px">Experience the next generation of WordPress.</h2>
-    <!-- /wp:heading -->
-
-    <!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.5"}}} -->
-    <p class="has-text-align-center" style="line-height:1.5">Frost is the ultimate WordPress theme for website builders.
-    </p>
-    <!-- /wp:paragraph -->
-  </div>
-  <!-- /wp:group -->
-
-  <!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|medium","left":"var:preset|spacing|large"},"margin":{"bottom":"var:preset|spacing|large"}}}} -->
-  <div class="wp-block-columns are-vertically-aligned-center" style="margin-bottom:var(--wp--preset--spacing--large)">
-    <!-- wp:column {"verticalAlignment":"center"} -->
-    <div class="wp-block-column is-vertically-aligned-center">
-      <!-- wp:image {"id":5489,"sizeSlug":"full","linkDestination":"none"} -->
-      <figure class="wp-block-image size-full"><img
-          src="http://localhost:8000/wp-content/themes/politics-wp-theme/assets/images/sample_1200x1200.jpg"
-          alt="Sample Image" class="wp-image-5489" /></figure>
-      <!-- /wp:image -->
-    </div>
-    <!-- /wp:column -->
-
-    <!-- wp:column {"verticalAlignment":"center"} -->
-    <div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"fontSize":"x-large"} -->
-      <h2 class="wp-block-heading has-x-large-font-size">Fresh patterns. Endless Layouts.</h2>
-      <!-- /wp:heading -->
-
-      <!-- wp:paragraph -->
-      <p>Frost is a masterpiece of design and functionality. It features a range of valuable elements, including hero
-        and portfolio sections, prominent call-to-action buttons, and customer testimonials.</p>
-      <!-- /wp:paragraph -->
-
-      <!-- wp:buttons -->
-      <div class="wp-block-buttons">
-        <!-- wp:button {"backgroundColor":"primary","style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}}}} -->
-        <div class="wp-block-button"><a
-            class="wp-block-button__link has-primary-background-color has-background wp-element-button"
-            style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--medium)">Get
-            Started →</a></div>
-        <!-- /wp:button -->
-      </div>
-      <!-- /wp:buttons -->
-    </div>
-    <!-- /wp:column -->
-  </div>
-  <!-- /wp:columns -->
-
-  <!-- wp:columns {"className":"is-style-columns-reverse","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|medium","left":"var:preset|spacing|large"},"margin":{"bottom":"var:preset|spacing|large"}}}} -->
-  <div class="wp-block-columns is-style-columns-reverse" style="margin-bottom:var(--wp--preset--spacing--large)">
-    <!-- wp:column {"verticalAlignment":"center"} -->
-    <div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"fontSize":"x-large"} -->
-      <h2 class="wp-block-heading has-x-large-font-size">One theme. Infinite designs.</h2>
-      <!-- /wp:heading -->
-
-      <!-- wp:paragraph -->
-      <p>Frost utilizes the potential of the editor to create link pages, serve as a powerful tool for building
-        relationships with clients, sharing valuable content, and boosting social media engagement.</p>
-      <!-- /wp:paragraph -->
-
-      <!-- wp:buttons -->
-      <div class="wp-block-buttons">
-        <!-- wp:button {"backgroundColor":"primary","style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}}}} -->
-        <div class="wp-block-button"><a
-            class="wp-block-button__link has-primary-background-color has-background wp-element-button"
-            style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--medium)">Get
-            Started →</a></div>
-        <!-- /wp:button -->
-      </div>
-      <!-- /wp:buttons -->
-    </div>
-    <!-- /wp:column -->
-
-    <!-- wp:column -->
-    <div class="wp-block-column"><!-- wp:image {"id":5490,"sizeSlug":"full","linkDestination":"none"} -->
-      <figure class="wp-block-image size-full"><img
-          src="http://localhost:8000/wp-content/themes/politics-wp-theme/assets/images/sample_1200x1200.jpg"
-          alt="Sample Image" class="wp-image-5490" /></figure>
-      <!-- /wp:image -->
-    </div>
-    <!-- /wp:column -->
-  </div>
-  <!-- /wp:columns -->
-
-  <!-- wp:columns {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"blockGap":{"top":"var:preset|spacing|medium","left":"var:preset|spacing|large"}}}} -->
-  <div class="wp-block-columns" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:column -->
-    <div class="wp-block-column"><!-- wp:image {"id":5492,"sizeSlug":"full","linkDestination":"none"} -->
-      <figure class="wp-block-image size-full"><img
-          src="http://localhost:8000/wp-content/themes/politics-wp-theme/assets/images/sample_1200x1200.jpg"
-          alt="Sample Image" class="wp-image-5492" /></figure>
-      <!-- /wp:image -->
-    </div>
-    <!-- /wp:column -->
-
-    <!-- wp:column {"verticalAlignment":"center"} -->
-    <div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"fontSize":"x-large"} -->
-      <h2 class="wp-block-heading has-x-large-font-size">Stylish websites. Simply made.</h2>
-      <!-- /wp:heading -->
-
-      <!-- wp:paragraph -->
-      <p>Whether you’re building a website for your business, personal brand, or creative project, Frost is the perfect
-        solution for anyone looking to launch a website quickly and efficiently.</p>
-      <!-- /wp:paragraph -->
-
-      <!-- wp:buttons -->
-      <div class="wp-block-buttons">
-        <!-- wp:button {"backgroundColor":"primary","style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}}}} -->
-        <div class="wp-block-button"><a
-            class="wp-block-button__link has-primary-background-color has-background wp-element-button"
-            style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--medium)">Get
-            Started →</a></div>
-        <!-- /wp:button -->
-      </div>
-      <!-- /wp:buttons -->
+    <div class="wp-block-column is-vertically-aligned-center"><!-- wp:video -->
+      <figure class="wp-block-video"><video controls
+          poster="http://localhost:8000/wp-content/uploads/2025/04/7Q4A1124.jpg"
+          src="https://res.cloudinary.com/dp16vnnxw/video/upload/v1743476485/Steven_Roberts_Campaign_Video_FINAL_c89fyr.mov"></video>
+      </figure>
+      <!-- /wp:video -->
     </div>
     <!-- /wp:column -->
   </div>
@@ -220,5 +85,4 @@
 <!-- /wp:group -->
 
 <div id="signup-form" class="iframe-wrapper"><iframe scrolling="no" allowfullscreen loading="lazy"
-    src="https://cdn.forms-content-1.sg-form.com/3d89d309-0e9d-11f0-8cd6-ba5d20bbf333"></iframe>
-</div>
+    src="https://cdn.forms-content-1.sg-form.com/3d89d309-0e9d-11f0-8cd6-ba5d20bbf333"></iframe></div>
