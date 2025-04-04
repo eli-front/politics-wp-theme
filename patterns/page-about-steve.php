99509fc1b665605bdf0de61d8cae5ca1cefae755
Eli Front <elifront23@gmail.com>
2025-04-04 01:28:26 -0500

    feat: WIP about page

---
+++ patterns/page-about-steve.php
@@ -0,0 +1,316 @@
+<?php
+
+/**
+ * Title: Starter Steve Roberts About Page 
+ * Slug: frost/page-about-steve
+ * Categories: frost-page
+ */
+?>
+<!-- wp:cover {"url":"http://localhost:8000/wp-content/uploads/2025/04/061117-1050-1.jpg","id":211,"dimRatio":20,"overlayColor":"primary","isUserOverlayColor":true,"focalPoint":{"x":0.62,"y":0},"minHeight":369,"minHeightUnit":"px","align":"full","style":{"spacing":{"blockGap":"0","margin":{"top":"var:preset|spacing|medium"}}}} -->
+<div class="wp-block-cover alignfull" style="margin-top:var(--wp--preset--spacing--medium);min-height:369px"><span
+    aria-hidden="true"
+    class="wp-block-cover__background has-primary-background-color has-background-dim-20 has-background-dim"></span><img
+    class="wp-block-cover__image-background wp-image-211" alt=""
+    src="http://localhost:8000/wp-content/uploads/2025/04/061117-1050-1.jpg" style="object-position:62% 0%"
+    data-object-fit="cover" data-object-position="62% 0%" />
+  <div class="wp-block-cover__inner-container">
+    <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained","contentSize":"1100px","wideSize":"1200px"}} -->
+    <div class="wp-block-group"><!-- wp:heading -->
+      <h2 class="wp-block-heading">About Steve Roberts</h2>
+      <!-- /wp:heading -->
+
+      <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}}} -->
+      <p style="font-style:normal;font-weight:400">Service To Community &amp; Country</p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+</div>
+<!-- /wp:cover -->
+
+<!-- wp:columns {"style":{"spacing":{"blockGap":{"left":"var:preset|spacing|x-small"}}}} -->
+<div class="wp-block-columns"><!-- wp:column {"width":"66.6%"} -->
+  <div class="wp-block-column" style="flex-basis:66.6%"><!-- wp:paragraph {"fontSize":"x-small"} -->
+    <p class="has-x-small-font-size">Senator Steven Roberts was sworn into office on Jan. 6, 2021, to serve the people
+      of the 5th Senatorial District and selected as the Democratic Whip for the Minority Caucus. He previously served
+      two terms in the Missouri House of Representatives, from 2017 to 2020. While in the House, he was selected by his
+      colleagues to serve as Chairman of the Missouri Legislative Black Caucus.</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:paragraph {"fontSize":"x-small"} -->
+    <p class="has-x-small-font-size">Senator Roberts is a fourth-generation St. Louis native, whose parents and
+      grandparents overcame segregation and discrimination to establish their careers. His mother, Dr. Eva Frazer,
+      became a distinguished doctor and his father a respected attorney, St. Louis alderman and successful businessman.
+      It is from his parents that he developed a strong sense of civic responsibility and social justice.</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:paragraph {"fontSize":"x-small"} -->
+    <p class="has-x-small-font-size">Senator Roberts holds a Bachelor of Science in Psychology and Communication Studies
+      from the University of Miami and received his law degree from Pepperdine University School of Law. While in law
+      school, his experiences included working in the U.S. Congress as a legislative intern; as a certified law clerk
+      with the District Attorney's Office in the City of Compton, California; assisting the Counsel to Secure Justice in
+      New Delhi, India, to help victims of violent sexual assault; and in his final semester, he worked with a clinic in
+      downtown Los Angeles helping refugees fleeing persecution in their country obtain asylum in the United States.</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:paragraph {"fontSize":"x-small"} -->
+    <p class="has-x-small-font-size">After graduating law school, he joined AP Wireless Infrastructure Partners, LLC as
+      a member of a team of attorneys responsible for managing over $200 million of company assets. Following the events
+      in Ferguson, Steve Roberts returned to serve his community as a prosecutor for the City of St. Louis where he
+      participated in over a hundred cases from minor weapons offenses to homicides.</p>
+    <!-- /wp:paragraph -->
+  </div>
+  <!-- /wp:column -->
+
+  <!-- wp:column {"width":"33.33%","style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}}} -->
+  <div class="wp-block-column" style="flex-basis:33.33%">
+    <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+    <div class="wp-block-group">
+      <!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"0","bottom":"0"}},"typography":{"fontSize":"26px"}}} -->
+      <h3 class="wp-block-heading" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0;font-size:26px">
+        Legislative Service</h3>
+      <!-- /wp:heading -->
+
+      <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+      <div class="wp-block-group">
+        <!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}},"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+        <h5 class="wp-block-heading"
+          style="margin-top:var(--wp--preset--spacing--x-small);font-style:normal;font-weight:600">Missouri State Senate
+        </h5>
+        <!-- /wp:heading -->
+
+        <!-- wp:paragraph {"fontSize":"x-small"} -->
+        <p class="has-x-small-font-size">Served from 2021-Present</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+
+      <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+      <div class="wp-block-group">
+        <!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}},"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+        <h5 class="wp-block-heading"
+          style="margin-top:var(--wp--preset--spacing--x-small);font-style:normal;font-weight:600">Missouri House of
+          Representatives</h5>
+        <!-- /wp:heading -->
+
+        <!-- wp:paragraph {"fontSize":"x-small"} -->
+        <p class="has-x-small-font-size">Served from 2016-2020</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+    <div class="wp-block-group">
+      <!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"0","bottom":"0"}},"typography":{"fontSize":"26px"}}} -->
+      <h3 class="wp-block-heading" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0;font-size:26px">
+        Military Service</h3>
+      <!-- /wp:heading -->
+
+      <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+      <div class="wp-block-group">
+        <!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}},"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+        <h5 class="wp-block-heading"
+          style="margin-top:var(--wp--preset--spacing--x-small);font-style:normal;font-weight:600">Missouri Air National
+          Guard</h5>
+        <!-- /wp:heading -->
+
+        <!-- wp:paragraph {"fontSize":"x-small"} -->
+        <p class="has-x-small-font-size">Captain in the JAG Corps</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+    <div class="wp-block-group">
+      <!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"0","bottom":"0"}},"typography":{"fontSize":"26px"}}} -->
+      <h3 class="wp-block-heading" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0;font-size:26px">
+        Statewide Committees</h3>
+      <!-- /wp:heading -->
+
+      <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+      <div class="wp-block-group">
+        <!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small","bottom":"0"}},"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+        <h5 class="wp-block-heading"
+          style="margin-top:var(--wp--preset--spacing--x-small);margin-bottom:0;font-style:normal;font-weight:600">
+          Missouri Military Preparedness and Enhancement Commission</h5>
+        <!-- /wp:heading -->
+      </div>
+      <!-- /wp:group -->
+
+      <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+      <div class="wp-block-group">
+        <!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small","bottom":"0"}},"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+        <h5 class="wp-block-heading"
+          style="margin-top:var(--wp--preset--spacing--x-small);margin-bottom:0;font-style:normal;font-weight:600">
+          Missouri Veterans' Commission</h5>
+        <!-- /wp:heading -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+    <div class="wp-block-group">
+      <!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"0","bottom":"0"}},"typography":{"fontSize":"26px"}}} -->
+      <h3 class="wp-block-heading" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0;font-size:26px">
+        Standing Committees</h3>
+      <!-- /wp:heading -->
+
+      <!-- wp:group {"style":{"spacing":{"blockGap":"0","margin":{"top":"0","bottom":"0"},"padding":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+      <div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0">
+        <!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small","bottom":"0"}},"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+        <h5 class="wp-block-heading"
+          style="margin-top:var(--wp--preset--spacing--x-small);margin-bottom:0;font-style:normal;font-weight:600">
+          Economic Development and Tax Policy</h5>
+        <!-- /wp:heading -->
+      </div>
+      <!-- /wp:group -->
+
+      <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+      <div class="wp-block-group">
+        <!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small","bottom":"0"}},"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+        <h5 class="wp-block-heading"
+          style="margin-top:var(--wp--preset--spacing--x-small);margin-bottom:0;font-style:normal;font-weight:600">
+          Fiscal Oversight</h5>
+        <!-- /wp:heading -->
+      </div>
+      <!-- /wp:group -->
+
+      <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+      <div class="wp-block-group">
+        <!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small","bottom":"0"}},"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+        <h5 class="wp-block-heading"
+          style="margin-top:var(--wp--preset--spacing--x-small);margin-bottom:0;font-style:normal;font-weight:600">
+          Insurance and Banking</h5>
+        <!-- /wp:heading -->
+      </div>
+      <!-- /wp:group -->
+
+      <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+      <div class="wp-block-group">
+        <!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small","bottom":"0"}},"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+        <h5 class="wp-block-heading"
+          style="margin-top:var(--wp--preset--spacing--x-small);margin-bottom:0;font-style:normal;font-weight:600">
+          Judiciary and Civil and Criminal Jurisprudence</h5>
+        <!-- /wp:heading -->
+      </div>
+      <!-- /wp:group -->
+
+      <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+      <div class="wp-block-group">
+        <!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small","bottom":"0"}},"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+        <h5 class="wp-block-heading"
+          style="margin-top:var(--wp--preset--spacing--x-small);margin-bottom:0;font-style:normal;font-weight:600">
+          Veterans, Military Affairs and Pensions</h5>
+        <!-- /wp:heading -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:column -->
+</div>
+<!-- /wp:columns -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"33.3%"} -->
+  <div class="wp-block-column" style="flex-basis:33.3%">
+    <!-- wp:image {"id":194,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+    <figure class="wp-block-image size-full"><img
+        src="http://localhost:8000/wp-content/uploads/2025/04/Air-Force-Photo.jpg" alt="" class="wp-image-194"
+        style="aspect-ratio:1;object-fit:cover" />
+      <figcaption class="wp-element-caption">Captain with the Missouri Air National Guard in the JAG Corps</figcaption>
+    </figure>
+    <!-- /wp:image -->
+  </div>
+  <!-- /wp:column -->
+
+  <!-- wp:column {"width":"66.6%"} -->
+  <div class="wp-block-column" style="flex-basis:66.6%"><!-- wp:paragraph {"fontSize":"x-small"} -->
+    <p class="has-x-small-font-size">In addition to his legislative duties, Sen. Roberts serves as a Captain with the
+      Missouri Air National Guard in the JAG Corps. He graduated from the United States Air Force’s Judge Advocate Staff
+      Officer Course (JASOC) and was selected as the Senior Ranking Officer for the entire JASOC class of approximately
+      50 attorneys. The JAG Corps is an elite group of law professionals who provide a full range of legal services to
+      the U.S. Air Force, the U.S. Space Force, airmen and guardians. He is also the managing partner of Roberts Law,
+      LLC.</p>
+    <!-- /wp:paragraph -->
+  </div>
+  <!-- /wp:column -->
+</div>
+<!-- /wp:columns -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":3} -->
+  <h3 class="wp-block-heading"><strong>Other Awards &amp; Recognitions</strong></h3>
+  <!-- /wp:heading -->
+
+  <!-- wp:columns {"verticalAlignment":null} -->
+  <div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center","width":"60%"} -->
+    <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:60%">
+      <!-- wp:list {"fontSize":"x-small"} -->
+      <ul class="wp-block-list has-x-small-font-size"><!-- wp:list-item -->
+        <li>2023 U.S. Black Chambers Inc. Power 50 Under 40, recognizing exceptional achievements of young Black
+          professionals</li>
+        <!-- /wp:list-item -->
+
+        <!-- wp:list-item -->
+        <li>Honored by the St. Louis Page Park Y Service Club International of the Monsanto Family YMCA</li>
+        <!-- /wp:list-item -->
+      </ul>
+      <!-- /wp:list -->
+    </div>
+    <!-- /wp:column -->
+
+    <!-- wp:column {"width":"40%"} -->
+    <div class="wp-block-column" style="flex-basis:40%">
+      <!-- wp:image {"id":240,"aspectRatio":"16/9","scale":"contain","sizeSlug":"full","linkDestination":"none"} -->
+      <figure class="wp-block-image size-full"><img
+          src="http://localhost:8000/wp-content/uploads/2025/04/usbc-award.webp" alt="" class="wp-image-240"
+          style="aspect-ratio:16/9;object-fit:contain" />
+        <figcaption class="wp-element-caption">U.S. Black Chambers</figcaption>
+      </figure>
+      <!-- /wp:image -->
+    </div>
+    <!-- /wp:column -->
+  </div>
+  <!-- /wp:columns -->
+
+  <!-- wp:columns {"verticalAlignment":null} -->
+  <div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center","width":"60%"} -->
+    <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:60%">
+      <!-- wp:list {"fontSize":"x-small"} -->
+      <ul class="wp-block-list has-x-small-font-size"><!-- wp:list-item -->
+        <li>2022 Friend of Higher Education award from the UMSL Alumni Association</li>
+        <!-- /wp:list-item -->
+
+        <!-- wp:list-item -->
+        <li>2015 Honored for Community Service by the North County Community Outreach Center</li>
+        <!-- /wp:list-item -->
+      </ul>
+      <!-- /wp:list -->
+    </div>
+    <!-- /wp:column -->
+
+    <!-- wp:column {"width":"40%"} -->
+    <div class="wp-block-column" style="flex-basis:40%">
+      <!-- wp:image {"id":239,"aspectRatio":"16/9","scale":"contain","sizeSlug":"full","linkDestination":"none"} -->
+      <figure class="wp-block-image size-full"><img
+          src="http://localhost:8000/wp-content/uploads/2025/04/umsl-award.webp" alt="" class="wp-image-239"
+          style="aspect-ratio:16/9;object-fit:contain" />
+        <figcaption class="wp-element-caption">UMSL</figcaption>
+      </figure>
+      <!-- /wp:image -->
+    </div>
+    <!-- /wp:column -->
+  </div>
+  <!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"127px"} -->
+<div style="height:127px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
